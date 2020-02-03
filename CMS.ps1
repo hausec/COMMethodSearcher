d4c3e96c1c4cb49e17dabb40e655c59b006018f9
Hausec <33264718+hausec@users.noreply.github.com>
2020-02-03 14:39:37 -0500

    uses CLSIDs instead of ProgIDs

--- CMS.ps1
+++ CMS.ps1
@@ -1,16 +1,19 @@
-﻿[CmdletBinding()]
+[CmdletBinding()]
 Param(
 [Int]$threads=20,
 [Parameter(Mandatory=$true)][String]$Term = $null,
 [Parameter(Mandatory=$true)][String]$Depth = $null)  
-$z = Get-ChildItem registry::HKEY_CLASSES_ROOT\WOW6432Node\CLSID -Recurse | Where-Object Name -match ProgID | ForEach GetValue ''
+$o = Get-ChildItem registry::HKEY_CLASSES_ROOT\WOW6432Node\CLSID
+$names = $o.Name
+$z = ForEach($name in $names){$name.Split("{}")[1]}
+#$z = "MMC20.Application"
 $y = $z | Get-Unique
 $ErrorActionPreference = 'silentlyContinue'
 If($Depth -eq 1)
 {
     ForEach($v in $y)
     {
-        $com = [activator]::CreateInstance([type]::GetTypeFromProgID("$v")) #Instantiate the COM Object
+        $com = [activator]::CreateInstance([type]::GetTypeFromCLSID("$v")) #Instantiate the COM Object
         $members = $com | Get-Member #Store all the members into a variable
         ForEach($member in $members)
         { 
@@ -25,7 +28,7 @@ If($Depth -eq 2)
 {
     ForEach($v in $y)
     {
-        $com = [activator]::CreateInstance([type]::GetTypeFromProgID("$v")) #Instantiate the COM Object
+        $com = [activator]::CreateInstance([type]::GetTypeFromCLSID("$v")) #Instantiate the COM Object
         $members = $com | Get-Member #Store all the members into a variable
         ForEach($member in $members)
         {
@@ -62,19 +65,20 @@ If($Depth -eq 3)
 {
     ForEach($v in $y)
     {
-        $com = [activator]::CreateInstance([type]::GetTypeFromProgID("$v")) #Instantiate the COM Object
+        $com = [activator]::CreateInstance([type]::GetTypeFromCLSID("$v")) #Instantiate the COM Object
         $members = $com | Get-Member #Store all the members into a variable
         ForEach($member in $members)
         {
-            $membernames = $member.Name 
+            
             If($member -notmatch 'Application' -and $member -notmatch 'Parent' -and $member -notmatch 'Cell' -and $member -notmatch 'Columns' -and $member -notmatch 'Rows')
             {
-                If($member.TypeName -match '__')
+                If($member.TypeName -match '__' -and $member.TypeName -notmatch '57da806104b8')
                 {
                     If($member -match $Term)
                     {
                         Write-Host "$v.$member"
-                    }          
+                    } 
+                    $membernames = $member.Name       
                     ForEach($membername in $membernames)
                     {
                         $m1members = $com.$membername | gm   
@@ -83,7 +87,7 @@ If($Depth -eq 3)
                             If($m1member -notmatch 'Application' -and $m1member -notmatch 'Parent' -and $m1member -notmatch 'Cell' -and $m1member -notmatch 'Columns' -and $m1member -notmatch 'Rows')
                             {
                                 If($m1member.TypeName -match '__')
-                                {          
+                                { 
                                     If($m1member -match $Term)
                                      {
                                         Write-Host "$v.$member.$m1member"
@@ -115,5 +119,4 @@ If($Depth -eq 3)
         }
     } 
 }
-
 
